9b8bc2e6d4e90e7537b13fa637c7128e8d631471
RichardLC99 <anyel_rian@hotmail.com>
2024-12-05 23:44:19 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -41,3 +41,39 @@ function hideModal() {
     var modal = document.getElementById('image-modal');
     modal.style.display = "none";
 }
+// Nueva función para mostrar imágenes al pasar el puntero 
+function showImagesOnHover() { 
+    var images = document.querySelectorAll('.image-container img'); 
+    images.forEach(function(img) { 
+        img.addEventListener('mouseenter', function() { 
+            img.style.opacity = '1'; 
+        }); 
+            img.addEventListener('mouseleave', function() { 
+                img.style.opacity = '0'; 
+        });
+    }); 
+} // Llamada a la función para mostrar imágenes al pasar el puntero al cargar la página 
+showImagesOnHover();
+
+function createHeart() { 
+    const heartsContainer = document.getElementById('hearts-container'); 
+    const heart = document.createElement('div'); 
+    heart.classList.add('heart-bubble'); 
+    heart.style.left = `${Math.random() * 100}%`; 
+    heartsContainer.appendChild(heart); 
+    
+    setTimeout(() => { 
+        heart.remove(); 
+    }, 5000); 
+} 
+setInterval(createHeart, 500);
+
+// Crear y animar corazones flotantes 
+function animateHearts() {
+     var hearts = document.querySelectorAll('.floating-hearts .heart'); 
+     hearts.forEach(function(heart) { 
+        heart.style.top = `${Math.random() * 100}vh`; 
+        heart.style.left = `${Math.random() * 100}vw`; 
+    }); 
+} 
+setInterval(animateHearts, 10000); // Actualizar la posición de los corazones cada 10 segundos
